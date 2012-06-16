549ebc7cbb5b3f654d35d0c8c078e1eb2a92dc33
thiago <thiagosantosleite@yahoo.com.br>
2012-06-16 12:17:52 -0700

    teste
    teste

--- agendaMedica/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-AgendaMedica::Application.config.secret_token = 'cc406fcfa9d57f97cb15ba663e2f0d43e3135fcc6d1a73d4d8ee40cc5c6697f4835bab27916421fed97c207b3a8a085ab5571d2850267696facc99f27517a911'
